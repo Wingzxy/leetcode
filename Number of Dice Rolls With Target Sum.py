5a9da165ef3029f449a1f653bc588341884605a3
Xingyang Zhou <xz15531@my.bristol.ac.uk>
2020-01-06 01:05:49 +0000

    1155. Number of Dice Rolls With Target Sum

---
+++ Number of Dice Rolls With Target Sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numRollsToTarget(self, d: int, f: int, target: int) -> int:
+        dp = [[0 for i in range(target + 1)] for j in range(d)]
+
+        for i in range(f):
+            if i + 1 <= target:
+                dp[0][i + 1] = 1
+
+        for i in range(1, d):
+            for j in range(1, target + 1):
+                k = 1
+                while j - k > 0 and k <= f:
+                    dp[i][j] += dp[i - 1][j - k]
+                    k += 1
+
+        return dp[-1][-1] % (1000000000+7)
